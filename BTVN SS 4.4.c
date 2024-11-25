32387c818ed04abaf25bcd83ebaf221868337b6c
Leducanh30112006 <le1duc2anh3@gmail.com>
2024-11-25 08:45:56 +0700

    Update BTVN SS 4.4.c

--- BTVN SS 4.4.c
+++ BTVN SS 4.4.c
@@ -5,14 +5,23 @@ int main() {
     printf("Nhap so thang: ");
     scanf("%d", &month);
     switch (month) {
-        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+        case 1: 
+        case 3: 
+        case 5: 
+        case 7: 
+        case 8: 
+        case 10: 
+        case 12:
             printf("Thang %d co 31 ngay.\n", month);
             break;
-        case 4: case 6: case 9: case 11:
+        case 4: 
+        case 6: 
+        case 9: 
+        case 11:
             printf("Thang %d co 30 ngay.\n", month);
             break;
         case 2:
-            printf("Thang %d co 28 hoac 29 ngay (nam nhuan).\n", month);
+            printf("Thang %d co 28 hoac 29 ngay.\n", month);
             break;
         default:
             printf("So thang khong hop le.\n");
